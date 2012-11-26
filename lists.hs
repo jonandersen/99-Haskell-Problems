a1e9c9c6eee929e5575cbdb1fdf6cd71cf02086c
Jon Andersen <jon.andersen88@gmail.com>
2012-11-26 18:32:30 +0100

    Problem 2

--- lists.hs
+++ lists.hs
@@ -2,3 +2,8 @@
 myLast = last
 testMyLast1 = myLast [1,2,3,4] == 4
 testMyLast2 = myLast ['x','y','z'] == 'z'
+
+-- 2
+myButLast = last . init
+testButMyLast1 = myButLast [1,2,3,4] == 3
+testButMyLast2 = myButLast ['a'..'z'] == 'y'
