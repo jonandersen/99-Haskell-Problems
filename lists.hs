aa213047806588ee1117dd74801d8dedc89c249e
Jon Andersen <jon.andersen88@gmail.com>
2012-11-27 11:29:17 +0100

    Problem 6

--- lists.hs
+++ lists.hs
@@ -1,21 +1,25 @@
 -- 1
 myLast = last
+
 testMyLast1 = myLast [1,2,3,4] == 4
 testMyLast2 = myLast ['x','y','z'] == 'z'
 
 -- 2
 myButLast = last . init
+
 testButMyLast1 = myButLast [1,2,3,4] == 3
 testButMyLast2 = myButLast ['a'..'z'] == 'y'
 
 -- 3
 elementAt xs x = last . fst $ splitAt x xs
+
 testElementAt1 = elementAt [1,2,3] 2 == 2
 testElementAt2 = elementAt "haskell" 5 == 'e'
 
 -- 4
 myLength [] = 0
 myLength (x:xs) = 1 + myLength xs
+
 testMyLength1 = myLength [123, 456, 789] == 3
 testMyLength2 = myLength "Hello, world!" == 13
 
@@ -25,4 +29,26 @@ myReverse (x:xs) = myReverse xs ++ [x]
 
 testMyReverse1 = myReverse "A man, a plan, a canal, panama!" == "!amanap ,lanac a ,nalp a ,nam A"
 testMyReverse2 = myReverse [1,2,3,4] == [4,3,2,1]
+
+-- 6 
+isPalindrome xs = reverse xs == xs
+
+testIsPalindrome1 = isPalindrome [1,2,3] == False
+testIsPalindrome2 = isPalindrome "madamimadam" == True
+testIsPalindrome3 = isPalindrome [1,2,4,8,16,8,4,2,1] == True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
